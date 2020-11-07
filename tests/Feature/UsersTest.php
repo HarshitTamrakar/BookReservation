c495800aa1c0aa9013c613804f553a251567bbbf
HarshitTamrakar <39623432+HarshitTamrakar@users.noreply.github.com>
2020-11-07 15:36:51 +0530

    added test for database entry

--- tests/Feature/UsersTest.php
+++ tests/Feature/UsersTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Book;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
@@ -11,16 +12,20 @@ class UsersTest extends TestCase
     use RefreshDatabase;
     
     public function test_only_logged_in_users_are_not_allowed(){
-        $response = $this->get('/home')->assertRedirect('/login');
+        $this->get('/home')->assertRedirect('/login');
     }
 
     public function test_only_logged_in_users_are_allowed(){
         $this->actingAs(User::factory()->create());
-        $response = $this->get('/home')->assertStatus(200);
+        $this->get('/home')->assertStatus(200);
     }
 
     public function test_books_are_added_successfully(){
-        
+        $user = User::factory()->create();
+        $this->actingAs($user);
+        $book = Book::factory()->create();
+        $this->get('/users/'.$user->id.'/books/'.$book->id)->assertRedirect('/home');
+        $this->assertCount(1, $user->books()->get());
     }
 
 }
